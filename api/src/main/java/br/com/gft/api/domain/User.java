856a8e952c836b715fb790c27a8f4fc7235b1c2a
logm <luiz-augusto.gomes@gft.com>
2022-07-13 14:41:09 -0300

    updating

--- api/src/main/java/br/com/gft/api/domain/User.java
+++ api/src/main/java/br/com/gft/api/domain/User.java
@@ -1,14 +1,15 @@
 package br.com.gft.api.domain;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_user")
 public class User {
 
 	@Id
@@ -16,67 +17,9 @@ public class User {
 	private Integer id;
 	private String name;
 
+//	Coluna unica - nao podera haver repeticao
 	@Column(unique = true)
 	private String email;
 	private String password;
-
-	public User() {
-	}
-
-	public User(Integer id, String name, String email, String password) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.password = password;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return Objects.equals(id, other.id);
-	}
 
 }
