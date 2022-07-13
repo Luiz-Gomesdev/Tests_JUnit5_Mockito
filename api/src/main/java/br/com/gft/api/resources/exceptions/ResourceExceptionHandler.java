f3b8b5282b278549d62203c4bea5a09c0f848330
logm <luiz-augusto.gomes@gft.com>
2022-07-13 17:19:45 -0300

    refactoring the project

--- api/src/main/java/br/com/gft/api/resources/exceptions/ResourceExceptionHandler.java
+++ api/src/main/java/br/com/gft/api/resources/exceptions/ResourceExceptionHandler.java
@@ -1,15 +1,14 @@
 package br.com.gft.api.resources.exceptions;
 
-import java.time.LocalDateTime;
-
-import javax.servlet.http.HttpServletRequest;
-
+import br.com.gft.api.services.exceptions.DataIntegratyViolationException;
+import br.com.gft.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import br.com.gft.api.services.exceptions.ObjectNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -19,6 +18,13 @@ public class ResourceExceptionHandler {
 		StandardError error = 
 				new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+
+	@ExceptionHandler(DataIntegratyViolationException.class)
+	public ResponseEntity<StandardError>dataIntegrityViolationException(DataIntegratyViolationException ex, HttpServletRequest request) {
+		StandardError error =
+				new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 
 }
