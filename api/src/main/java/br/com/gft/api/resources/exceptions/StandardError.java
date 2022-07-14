820f18aaf87882eeece507263f9f50ee20a4f701
logm <luiz-augusto.gomes@gft.com>
2022-07-14 17:28:08 -0300

    removendo anotacoes nao utilizadas

--- api/src/main/java/br/com/gft/api/resources/exceptions/StandardError.java
+++ api/src/main/java/br/com/gft/api/resources/exceptions/StandardError.java
@@ -1,12 +1,12 @@
 package br.com.gft.api.resources.exceptions;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
+@AllArgsConstructor
 public class StandardError {
 
 	private LocalDateTime timestamp;
@@ -14,16 +14,6 @@ public class StandardError {
 	private String error;
 	private String path;
 
-	public StandardError() {
-	}
-
-	public StandardError(LocalDateTime timestamp, Integer status, String error, String path) {
-		super();
-		this.timestamp = timestamp;
-		this.status = status;
-		this.error = error;
-		this.path = path;
-	}
 
 
 }
